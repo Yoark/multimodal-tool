c9e480a871f658b8d23b859a8a1dff04362fd971
JT <wengyagami@hotmail.com>
2020-11-19 15:03:54 -0800

    add react notes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Nav from './views/Nav'
 import Landing from './views/Landing'
 import AnotherPage from './views/AnotherPage'
 import './App.css'
@@ -6,6 +7,7 @@ import './App.css'
 function App() {
   return (
     <Router>
+        <Nav />
         <Route path='/' exact component={Landing} />
         {/* you can access the following page by visiting http://localhost:3000/test */}
         <Route path='/test' exact component={AnotherPage} />
---
+++ src/views/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      Nav bar here
+    </div>
+  )
+}
+
+export default Nav
